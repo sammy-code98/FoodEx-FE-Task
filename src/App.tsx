d2871cfca08fad4a55e0e48135440202a66c5d32
sammy-code98 <sammysmart98@yahoo.com>
2025-01-21 18:42:32 +0100

    feat: initial commit - create react app with vite - scaldold project - add tailwind css - set up color system

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-chow-orange">hello world</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "chow-black": "#0E0E0E",
+        "chow-white": "#FFFFFF",
+        "chow-green": "#009F79",
+        "chow-orange": "#D87023",
+        "chow-snow": "#FFF8F2",
+        "chow-blue": "#5F63E1",
+      },
+    },
+  },
+  plugins: [],
+};
